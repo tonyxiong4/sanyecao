8f86ee8eb9e4ed34a860a265f794d2471bc1c9e0
tonyxiong4 <1224414470@qq.com>
2018-05-07 15:15:52 +0800

    空控制器可以放一些错误页面

---
+++ application/index/controller/Error.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\index\controller;
+use think\Controller;
+
+class Error extends Controller
+{
+    public function _empty()
+    {
+        echo "nidayede";//这里可以添加一个错误页面404
+    }
+}
