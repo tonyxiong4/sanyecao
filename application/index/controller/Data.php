3394dd0161c63197cbcae8efe0dfd1fa0bea44ba
tonyxiong4 <1224414470@qq.com>
2018-05-08 01:14:21 +0800

    我的客户

--- application/index/controller/Data.php
+++ application/index/controller/Data.php
@@ -3,7 +3,7 @@
  * @Author: tony
  * @Date:   2018-05-05 22:54:09
  * @Last Modified by:   tony
- * @Last Modified time: 2018-05-06 18:00:23
+ * @Last Modified time: 2018-05-08 00:59:45
  */
 
 namespace app\index\controller;
@@ -34,16 +34,48 @@ class Data extends Controller
 		return [];
 	}
 
+	/**
+	 * [addCustomser 添加客户]
+	 */
 	public function addCustomser()
 	{
 		$param=$this->request->param();
-		dump($param);
+		$data=[];
 
+		if($param){
+			$data['name']=$param['custname'];
+			$data['phone']=$param['custphone'];
+			$data['company']=$param['custcompany'];
+			$data['belonguid']=session('userinfo.uid');
+		}
+		$result=Db::name('customer')->insert($data);
+		if($result){
+			$mes['code']=200;
+			$mes['msg']="添加成功";
+		}else{
+			$mes['code']=-1;
+			$mes['msg']="添加失败";
+		}
+		return json($mes);
 	}
 
+	/**
+	 * [delCustomser 删除客户]
+	 * @return [type] [description]
+	 */
 	public function delCustomser()
 	{
 		$param=$this->request->param();
-		dump($param);
+		$id=$param['id'];
+		$where['id']=$id;
+		$result=Db::name('customer')->where($where)->setField('status',9);
+		if($result>0){
+			$mes['code']=200;
+			$mes['msg']="删除成功";
+		}else{
+			$mes['code']=-1;
+			$mes['msg']="删除失败";
+		}
+		return json($mes);
 	}
 }
