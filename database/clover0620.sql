17172916a3135bd93cfc297fde4117c94354fc71
tonyxiong4 <1224414470@qq.com>
2018-06-20 13:36:43 +0800

    最新

--- database/clover0620.sql
+++ database/clover0620.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v11.25 (64 bit)
-MySQL - 5.1.73 : Database - hdm310089504_db
+MySQL - 5.1.73 : Database - clover
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 5.1.73 : Database - hdm310089504_db
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`hdm310089504_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`clover` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `hdm310089504_db`;
+USE `clover`;
 
 /*Table structure for table `syc_customer` */
 
